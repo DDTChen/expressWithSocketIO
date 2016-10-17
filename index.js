3051421bf4ffdf6c1e0a5bf0d09e17e3aa02a8e2
DDTChen <ddttchen@gmail.com>
2016-10-17 15:35:12 +0800

    setup socket.io

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+
+//websocket
+var io = require('socket.io')(http);
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+//websocket
+io.on('connection', function(socket) {
+  console.log('a user connected');
+});
+
+http.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
